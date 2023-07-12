29004e31641d1dc4bb7d6de039efa07b5558ee87
Alexander Rognli <85561236+Al-roG@users.noreply.github.com>
2023-07-12 15:58:56 +0200

    Dev ops (#2)
    * Minor updates to ReadMe.
    
    * Minor typo fixes
    
    * Things to do, things to squash
    
    * Remove skip action
    
    * Commit version 2.2
    
    * Remove annoying formatting
    
    * Added path validation and hash measurement for a better experience
    
    * Version 2.3 commited to devOps
    
    * Minor corrections. Version 2.4 commit
    
    ---------
    
    Co-authored-by: Alexander Rognli <alexander.rognli@advania.com>

--- Public Scripts/ChecksumTool.ps1
+++ Public Scripts/ChecksumTool.ps1
@@ -6,57 +6,80 @@
 
 Write-Host "#####################################" -ForegroundColor Cyan 
 Write-Host "### " -F Cyan -NoNewline; `
-Write-Host "Rognli Checksum Tool | v2.1 |" -F Yellow -NoNewline; `
+Write-Host "Rognli Checksum Tool | v2.4 |" -F Yellow -NoNewline; `
 Write-Host " ###" -F Cyan
 Write-Host "#####################################" -ForegroundColor Cyan
 Write-Host ""
 Write-Host ""
 
 # Manual or Compare selection
-Write-Host " Do you wish to (C)ompare two files or (M)anually input the hash? " -F Yellow -NoNewline; $UserSelection = Read-Host
-Write-Host ""
+
+    Do { Write-Host " Do you wish to (C)ompare two files or (M)anually input the hash? " -F Yellow -NoNewline; $UserSelection = Read-Host }
+    While (( $UserSelection -notlike "M" ) -and ($UserSelection -notlike "C"))
 
 # Compare hashes between two files
 
-if ($UserSelection -eq 'C')
-{ 
-    Write-Host " Input path to source file: " -F Yellow -NoNewline; $SourceFilePath = Read-Host
-    $SourceFile = $SourceFilePath -replace '"', ''
+If ($UserSelection -eq 'C') { 
+
+# Fetch, validate and trim the source file path
+
+    Do { Write-Host " Input path to source file (within quotation marks): " -F Yellow -NoNewline; $SourceFilePath = Read-Host }
+    While (( $SourceFilePath -Like '') -or ( Test-Path -Path $SourceFilePath -IsValid ))
+    $SourceFile = $SourceFilePath -Replace '"', ''  
+    
     Write-Host ""
-    Write-Host " Input path to comparison file: " -F Yellow -NoNewline; $CompareFilePath = Read-Host
-    $CompareFile = $CompareFilePath -replace '"', ''
+
+# Fetch, validate and trim the comparison file path
+
+    Do { Write-Host " Input path to comparison file (within quotation marks): " -F Yellow -NoNewline; $CompareFilePath = Read-Host }
+    While (( $CompareFilePath -Like '' ) -or ( Test-Path -Path $CompareFilePath -IsValid ))
+    $CompareFile = $CompareFilePath -Replace '"', ''
     Write-Host ""
-    if (( Get-FileHash $SourceFile ).Hash -eq (Get-FileHash $CompareFile).Hash )
-    {  Write-host "The file hash is correct. The file is safe to use." -F Green }
-    else
+
+# Execute the hash comparison 
+
+    If (( Get-FileHash $SourceFile ).Hash -eq (Get-FileHash $CompareFile).Hash )
+    { Write-host "The file hash is correct. The file is safe to use." -F Green }
+    Else
     { Write-Host "The file hash is a mismatch. Do NOT use this file." -F Red }
 }
 
 # Manual hash input comparison
 
-if ($UserSelection -eq 'M')
-{ 
-    Write-Host " Input source hash: " -F Yellow -NoNewline; $SourceHash = Read-Host
+If ($UserSelection -eq 'M') { 
+
+# Fetch and measure the manual source hash
+
+    Do { Write-Host " Input source hash: " -F Yellow -NoNewline; $SourceHash = Read-Host }
+    While (( $SourceHash -Like '' ) -or ( $SourceHash.Length -lt 64 ))
     Write-Host ""
-    Write-Host " Input path to comparison file: " -F Yellow -NoNewline; $CompareFilePath = Read-Host
-    $CompareFile = $CompareFilePath -replace '"', ''
+
+# Fetch, validate and trim the comparison file path
+
+    Do { Write-Host " Input path to comparison file (within quotation marks): " -F Yellow -NoNewline; $CompareFilePath = Read-Host }
+    While (( $CompareFilePath -Like '' ) -or ( Test-Path -Path $CompareFilePath -IsValid ))
+    $CompareFile = $CompareFilePath -Replace '"', ''
     Write-Host ""
-    if (( Get-FileHash $CompareFile ).Hash -eq $SourceHash )
-    {  Write-host "The file hash is correct. The file is safe to use." -F Green }
-    else
+
+# Execute the hash comparison
+
+    If (( Get-FileHash $CompareFile ).Hash -eq $SourceHash )
+    { Write-host "The file hash is correct. The file is safe to use." -F Green }
+    Else
     { Write-Host "The file hash is a mismatch. Do NOT use this file." -F Red }
 }
 
 # End the script
 
+Write-Host ""
 $Prompt = "Do you wish to (E)xit or (R)eturn? "
 
-do {
+Do {
     Write-Host $Prompt -F Cyan -NoNewline; $ExitChoice = Read-Host
-    if ($ExitChoice -eq 'R')
-    {Return}
-    if ($ExitChoice -eq 'E')
-    {Exit}
-} while ( 
+    If ($ExitChoice -eq 'R')
+    { Return }
+    If ($ExitChoice -eq 'E')
+    { Exit }
+} While ( 
     $ExitChoice -ne 'R' -or 'E'
 )
--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-I will periodically put generic scripts in this public repo.
+# About
+Writing Powershell scripts for fun!
 
-If you found a specific script useful, please let me know :)
+I will periodically put up some generic scripts in my public repo.
 
-Please do not remove the credits!
+Check in from time to time ;-)
+
+# Credits
+If you found a specific script useful, please let me know.
+Also, please do not remove credits from my scripts, but feel free to use them.
+
+# Useful Links
+https://learn.microsoft.com/en-us/powershell/
