16e3804721efdb7ed37318acd2b156b9971d182d
Alexander Rognli <alexander.rognli@advania.com>
2023-07-12 11:21:55 +0200

    Version 2.0 - Added the option to use comparison or manual input

--- Public Scripts/ChecksumTool.ps1
+++ Public Scripts/ChecksumTool.ps1
@@ -1,25 +1,54 @@
-# A simple tool to make sure the checksum hasn't been altered
+## Presentation & Credits
+# This is a simple tool to make sure the checksum hasn't been altered on a file.
+# Either input two file paths if you wish to compare two files.
+# If you wish you compare a file towards a pre-fetched hash, select Manual input.
 
-Write-Host "############################" -ForegroundColor Cyan 
+
+Write-Host "###############################" -ForegroundColor Cyan 
 Write-Host "### " -F Cyan -NoNewline; `
-Write-Host "Rognli Checksum Tool" -F Yellow -NoNewline; `
+Write-Host "Rognli Checksum Tool v2" -F Yellow -NoNewline; `
 Write-Host " ###" -F Cyan
-Write-Host "############################" -ForegroundColor Cyan
+Write-Host "###############################" -ForegroundColor Cyan
 Write-Host ""
 Write-Host ""
 
-Write-Host " Input filepath to downloaded file: " -F Yellow -NoNewline; $FileToCompare = Read-Host
-$FilePath = $FileToCompare -replace '"', ''
-Write-Host ""
-Write-Host " Input the file hash from the source: " -F Yellow -NoNewline; $TrueHash = Read-Host 
+# Manual or Compare selection
+Write-Host " Do you wish to (C)ompare two files or (M)anually input the hash? " -F Yellow -NoNewline; $UserSelection = Read-Host
 Write-Host ""
 
+# Compare hashes between two files
 
-if (( Get-FileHash $FilePath ).Hash -eq $TrueHash)
-{ Write-host "The file hash is correct. The file is safe to use." -ForegroundColor Green }
-else 
-{ Write-Host "The file hash is a mismatch. Do NOT use this file." -ForegroundColor Red }
+if ($UserSelection -eq 'C')
+{ 
+    Write-Host " Input path to source file: " -F Yellow -NoNewline; $SourceFilePath = Read-Host
+    $SourceFile = $SourceFilePath -replace '"', ''
+    Write-Host ""
+    Write-Host " Input path to comparison file: " -F Yellow -NoNewline; $CompareFilePath = Read-Host
+    $CompareFile = $CompareFilePath -replace '"', ''
+    Write-Host ""
+    if (( Get-FileHash $SourceFile ).Hash -eq (Get-FileHash $CompareFile).Hash )
+    {  Write-host "The file hash is correct. The file is safe to use." -ForegroundColor Green }
+    else
+    { Write-Host "The file hash is a mismatch. Do NOT use this file." -ForegroundColor Red }
+}
+
+# Manual hash input comparison
+
+if ($UserSelection -eq 'M')
+{ 
+    Write-Host " Input source hash: " -F Yellow -NoNewline; $SourceHash = Read-Host
+    Write-Host ""
+    Write-Host " Input path to comparison file: " -F Yellow -NoNewline; $CompareFilePath = Read-Host
+    $CompareFile = $CompareFilePath -replace '"', ''
+    Write-Host ""
+    if (( Get-FileHash $CompareFile ).Hash -eq $SourceHash )
+    {  Write-host "The file hash is correct. The file is safe to use." -ForegroundColor Green }
+    else
+    { Write-Host "The file hash is a mismatch. Do NOT use this file." -ForegroundColor Red }
+}
+
+# End the script
 
 Write-Host ""
-Write-Host "Press any key to exit..." -ForegroundColor Cyan -NoNewline; Read-Host
+Write-Host "Press enter to exit..." -ForegroundColor Cyan -NoNewline; Read-Host
 Exit
